7c0e86ea42bfa100aa05a90ffa23867fdff6cef4
Mugundh <mugundhjb@gmail.com>
2024-01-31 22:03:02 +0530

    Checking User login with cookies

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'mugundh';
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect('mongodb+srv://mugundhjb:mugundh123@cluster0.cin9jqd.mongodb.net/?retryWrites=true&w=majority')
 app.post("/register", async (req, res) => {
@@ -41,6 +43,14 @@ app.post("/login", async (req, res) => {
         }
 });
 
+app.get("/profile", (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if(err) throw err;
+        res.json(info);
+    })
+    res.json(req.cookies);
+})
 app.listen(4000);
 
 // mongodb+srv://mugundhjb:mugundh123@cluster0.cin9jqd.mongodb.net/?retryWrites=true&w=majority
